03138a5dca3184f3cdc4606eadb266d6c995161f
yourfriend143 <ramukakayourdada143@gmail.com>
2025-10-08 08:27:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "23283708"
+api_hash = "7805011fb84729023531f0fa3f000bec"
+bot_token = "8273750812:AAGvS5NlWItov_ePlmwHGx11nmhqJ-YcGMg"
+auth_users = [6481888008]
